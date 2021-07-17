bd6cfd44ea3706075159ea66110d51971edaea0b
Bilal Khalid <bilalkhalid1000@gmail.com>
2021-07-17 21:24:10 +0500

    Added webview and added back button action listern in component.

--- App.js
+++ App.js
@@ -1,13 +1,52 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { StyleSheet, BackHandler, Alert } from 'react-native';
+import { WebView } from 'react-native-webview';
+import Constants from 'expo-constants'
 
 export default function App() {
+  const [cangoback, setCangoback] = useState(false);
+  const [webview, setWebview] = useState(null);
+
+  useEffect(() => {
+    // adding event listner on back button press
+    const backHandler = BackHandler.addEventListener(
+      "hardwareBackPress",
+      backAction
+    );
+
+    return () => backHandler.remove();
+  });
+
+  const backAction = () => {
+    // checking if app can navigate to a previous screen
+    if(cangoback) {
+      // navigating to a previous screen
+      webview.goBack();
+    } else {
+      // showing alert app cannot navigate to previous screen, to exit app
+      Alert.alert("Exit", "Are you sure you want to exit app?", [
+        {
+          text: "Cancel",
+          onPress: () => null,
+          style: "cancel"
+        },
+        // exiting app on yes
+        { text: "YES", onPress: () => BackHandler.exitApp() }
+      ]);
+    }
+    return true;
+  };
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <WebView
+      ref={ref => setWebview(ref)}
+      style={styles.container}
+      source={{ uri: 'https://propertyclick.pk/' }}
+      onNavigationStateChange={(navState) => {
+        // Keep track of going back navigation for back button press
+        setCangoback(navState.canGoBack);
+      }}
+    />
   );
 }
 
@@ -17,5 +56,6 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    marginTop: Constants.statusBarHeight,
   },
 });
